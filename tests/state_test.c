f7d4907f35bcec1c9e0cfa3ad7d848585b1b8571
Kostas Chatzikokolakis <kostas@chatzi.org>
2021-03-09 15:18:12 +0200

    state_test

--- tests/state_test.c
+++ tests/state_test.c
@@ -8,28 +8,52 @@
 
 #include "state.h"
 
-void test_state_create(void) {
+void test_state_create() {
 
 	State state = state_create();
-	TEST_CHECK(state != NULL);
+	TEST_ASSERT(state != NULL);
 
 	StateInfo info = state_info(state);
+	TEST_ASSERT(info != NULL);
 
-	TEST_CHECK(info->current_portal == 0);
-	TEST_CHECK(info->wins == 0);
-	TEST_CHECK(info->playing);
-	TEST_CHECK(!info->paused);
+	TEST_ASSERT(info->current_portal == 0);
+	TEST_ASSERT(info->wins == 0);
+	TEST_ASSERT(info->playing);
+	TEST_ASSERT(!info->paused);
 
 	// Προσθέστε επιπλέον ελέγχους
 }
 
-// Προσθέστε επιπλέον συναρτήσεις
-// ...
+void test_state_update() {
+	State state = state_create();
+	TEST_ASSERT(state != NULL && state_info(state) != NULL);
+
+	// Πληροφορίες για τα πλήκτρα (αρχικά κανένα δεν είναι πατημένο)
+	struct key_state keys = { false, false, false, false, false, false };
+	
+	// Χωρίς κανένα πλήκτρο, ο χαρακτήρας μετακινείται 7 pixels μπροστά
+	Rectangle old_rect = state_info(state)->character->rect;
+	state_update(state, &keys);
+	Rectangle new_rect = state_info(state)->character->rect;
+
+	TEST_ASSERT( new_rect.x == old_rect.x + 7 && new_rect.y == old_rect.y );
+
+	// Με πατημένο το δεξί βέλος, ο χαρακτήρας μετακινείται 12 pixes μπροστά
+	keys.right = true;
+	old_rect = state_info(state)->character->rect;
+	state_update(state, &keys);
+	new_rect = state_info(state)->character->rect;
+
+	TEST_CHECK( new_rect.x == old_rect.x + 12 && new_rect.y == old_rect.y );
+
+	// Προσθέστε επιπλέον ελέγχους
+}
 
 
 // Λίστα με όλα τα tests προς εκτέλεση
 TEST_LIST = {
 	{ "test_state_create", test_state_create },
+	{ "test_state_update", test_state_update },
 
 	{ NULL, NULL } // τερματίζουμε τη λίστα με NULL
 };
